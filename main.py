18d8a5ae6229aa250e173467a3473f60048fa9b1
Theodor Tolstoy <ttolstoy@ebsco.com>
2018-05-17 11:34:03 +0200

    added identifiers and contributors

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import json, sys, requests,pymarc,time, glob, uuid
 print("Script starting")
 
-def getContributors(marcRecord):
-    primary = True
-    for fieldName in ['100','110','111']:
-         for field in record.get_fields(fieldName):
-             yield {'name':field.format_field(), 'contributorNameTypeId':'', 'primary':primary}
+def getContributors(marcRecord, contributorNameTypes):
+    a = { '100' : next(f['id'] for f in contributorNameTypes if f['name'] == 'Personal name') 
+         ,'110' : next(f['id'] for f in contributorNameTypes if f['name'] == 'Corporate name')
+         ,'111' : next(f['id'] for f in contributorNameTypes if f['name'] == 'Meeting name') }    
+    for fieldTag in a.keys():
+         for field in record.get_fields(fieldTag):
+             yield {'name':field.format_field(), 'contributorNameTypeId': a[fieldTag], 'primary':'false'}
+
+def getIdentifiers(marcRecord, identifierTypes):
+    a = {'020' : next(f['id'] for f in identifierTypes if f['name'] == 'ISBN')
+         ,'022' : next(f['id'] for f in identifierTypes if f['name'] == 'ISSN')}
+    for fieldTag in a.keys():
+        for field in record.get_fields(fieldTag):
+            yield {'identifierTypeId':a[fieldTag], 'value' : field.format_field() }
 
 def getFoliIdentifierTypes(okapiUrl, okapiHeaders):
     path = '/identifier-types?limit=100&query=cql.allRecords=1%20sortby%20name'
     req =  requests.get(okapiUrl+path, headers = okapiHeaders)
-    return json.loads(req.text)
+    return json.loads(req.text)['identifierTypes']
 
 def getFolioContributorTypes(okapiUrl, okapiHeaders):
     path = '/contributor-types?limit=100&query=cql.allRecords=1 sortby name'
@@ -20,12 +29,13 @@ def getFolioContributorTypes(okapiUrl, okapiHeaders):
 def getFolioContributorNameTypes(okapiUrl, okapiHeaders):
     path = '/contributor-name-types?limit=100&query=cql.allRecords=1 sortby ordering'
     req =  requests.get(okapiUrl+path, headers = okapiHeaders)
-    return json.loads(req.text)
+    return json.loads(req.text)['contributorNameTypes']
 
-def parseBibRecord(marcRecord,folioRedord):
+def parseBibRecord(marcRecord,folioRedord, contributorNameTypes, identifierTypes):
     folioRedord['title'] = marcRecord.title()
     folioRedord['source'] = "Test source"
-    folioRedord['contributors'] = list(getContributors(marcRecord))
+    folioRedord['contributors'] = list(getContributors(marcRecord, contributorNameTypes))
+    folioRedord['identifiers'] = list(getIdentifiers(marcRecord, identifierTypes))
     return folioRedord
     
 def folioRecordTemplate(id):
@@ -54,11 +64,21 @@ okapiHeaders =  {'x-okapi-token': okapiToken,'x-okapi-tenant': tenantId, 'conten
 holdings = 0
 records = 0
 start = time.time()
-foliIdentifierTypes = getFoliIdentifierTypes(okapiUrl, okapiHeaders)
 files = onlyfiles = [f for f in listdir(sys.argv[1]) if isfile(join(sys.argv[1], f))]
-bufsize = 1 
+bufsize = 1
+
 print("Files to process:")
 print(json.dumps(files, sort_keys=True, indent=4))
+print("fetching foliIdentifierTypes")
+foliIdentifierTypes = getFoliIdentifierTypes(okapiUrl, okapiHeaders)
+print(foliIdentifierTypes)
+print("done")
+print("fetching CreatorTypes")
+contributorNameTypes = getFolioContributorNameTypes(okapiUrl, okapiHeaders)
+print(contributorNameTypes)
+print("done")
+
+
 for f in files:
     from pymarc import MARCReader
     with open(sys.argv[1]+f, 'rb') as fh:
@@ -69,12 +89,12 @@ for f in files:
                 if record['004']:
                     holdings += 1
                 else:
-                    fRec = parseBibRecord(record, folioRecordTemplate(uuid.uuid4()))
+                    fRec = parseBibRecord(record, folioRecordTemplate(uuid.uuid4()), contributorNameTypes, foliIdentifierTypes)
                     if records % 10000 == 0 :
                         elapsed = records/(time.time() - start)
                         print(json.dumps(fRec, sort_keys=True, indent=4))
                         printTemplate = "Records/second: {}\tHoldings: {}\tTotal records:{}\tCurrent file:{}" 
                         print(printTemplate.format(elapsed, holdings, records,f)) #,end='\r')
             except:
-                println('error!')
+                print('error!')
 print("Script finished")
