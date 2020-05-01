210d0a1828f9a3f7e2333694df0fe0ac77780cee
Theodor Tolstoy <ttolstoy@ebsco.com>
2020-05-01 23:29:18 +0200

    object orient and filter on 945s

--- main_bibs.py
+++ main_bibs.py
@@ -2,6 +2,7 @@
 import argparse
 import json
 import logging
+import csv
 import sys
 import traceback
 from os import listdir
@@ -14,8 +15,117 @@ from marc_to_folio import RulesMapper
 from marc_to_folio.marc_processor import MarcProcessor
 
 
-def main():
-    logging.basicConfig(level=logging.CRITICAL)
+class Worker:
+    """Class that is responsible for the acutal work"""
+
+    def __init__(self, folio_client, results_file, args):
+        # msu special case
+        self.args = args
+        self.results_file_path = results_file
+        self.allowed_locs = self.setup_allowed(args)
+        self.files = [
+            f
+            for f in listdir(args.source_folder)
+            if isfile(join(args.source_folder, f))
+        ]
+        self.folio_client = folio_client
+        print(f"Files to process: {len(self.files)}")
+        print(json.dumps(self.files, sort_keys=True, indent=4))
+
+        self.mapper = RulesMapper(folio_client, args.results_folder)
+        print("Rec./s\t\tTot. recs\t\t")
+        self.failed_records = list()
+        self.failed_files = list()
+        self.filtered_out_locations = {}
+        print("Init done")
+
+    def work(self):
+        print("Starting....")
+        with open(self.results_file_path, "w+") as results_file:
+            processor = MarcProcessor(
+                self.mapper, self.folio_client, results_file, self.args
+            )
+            for file_name in self.files:
+                try:
+                    with open(join(sys.argv[1], file_name), "rb") as marc_file:
+                        reader = MARCReader(marc_file, "rb", permissive=True)
+                        reader.hide_utf8_warnings = True
+                        if self.args.force_utf_8:
+                            print("FORCE UTF-8 is set to TRUE")
+                            reader.force_utf8 = True
+                        print(f"running {file_name}")
+                        self.read_records(reader, processor)
+                except Exception as exception:
+                    print(exception)
+                    traceback.print_exc()
+                    print(file_name)
+            # wrap up
+            self.wrap_up(processor)
+
+    def read_records(self, reader, processor):
+        for record in reader:
+            if record is None:
+                print(
+                    f"Current chunk: {reader.current_chunk} "
+                    " was ignored because the following exception: ",
+                    f"{reader.current_exception}",
+                )
+                self.failed_records.append(reader.current_chunk)
+            else:
+                if not self.allowed_locs or not self.msu_filter(record):
+                    processor.process_record(record)
+
+    def wrap_up(self, processor):
+        print("Done. Wrapping up...")
+        processor.wrap_up()
+        print("Failed files:")
+        print(json.dumps(self.failed_files, sort_keys=True, indent=4))
+        print(f"Failed records ({len(self.failed_records)}):")
+        print(json.dumps(self.failed_records, indent=4))
+        print("filter_by_location_results:")
+        print(json.dumps(self.filtered_out_locations, indent=4))
+        print("done")
+
+    def msu_filter(self, record):
+        f945s = record.get_fields("945")
+        marc_locs = []
+        for f in f945s:
+            for sf in f.get_subfields("l"):
+                marc_locs.append(sf)
+        has_allowed = any(
+            marc_loc for marc_loc in marc_locs if marc_loc in self.allowed_locs
+        )
+        for loc in marc_locs:
+            if loc in self.allowed_locs:
+                add_stats(self.filtered_out_locations, f"{loc} - ALLOWED")
+            else:
+                add_stats(self.filtered_out_locations, f"{loc} - FILTERED OUT")
+        if has_allowed:
+            return False
+        else:
+            add_stats(self.filtered_out_locations, "TOTAL FILTERED OUT")
+            return True
+
+    def setup_allowed(self, args):
+        if args.msu_locations_path:
+            csv.register_dialect("tsv", delimiter="\t")
+            with open(args.msu_locations_path) as loc_file:
+                self.locations_map = list(csv.DictReader(loc_file, dialect="tsv"))
+                allowed_locs = list(
+                    l["iii_code"]
+                    for l in self.locations_map
+                    if l["folio_code"] != "remove" and l["iii_code"]
+                )
+                print(
+                    f"{len(allowed_locs)} allowed locations fetched:\n{json.dumps(allowed_locs)}"
+                )
+                return allowed_locs
+        else:
+            return []
+
+
+def parse_args():
+    """Parse CLI Arguments"""
     parser = argparse.ArgumentParser()
     parser.add_argument("source_folder", help="path to marc records folder")
     parser.add_argument("results_folder", help="path to Instance results folder")
@@ -32,6 +142,9 @@ def main():
         action="store_true",
     )
     parser.add_argument(
+        "-msu_locations_path", "-f", help=("filter records based on MSU rules")
+    )
+    parser.add_argument(
         "-postgres_dump",
         "-p",
         help=("results will be written out for Postgres" "ingestion. Default is JSON"),
@@ -46,8 +159,17 @@ def main():
         help=("Validate JSON data against JSON Schema"),
         action="store_true",
     )
-
     args = parser.parse_args()
+    return args
+
+
+def main():
+    """Main Method. Used for bootstrapping. """
+    # Parse CLI Arguments
+    args = parse_args()
+
+    logging.basicConfig(level=logging.CRITICAL)
+
     results_file = join(args.results_folder, "folio_instances.json")
     print("\tresults will be saved at:\t", args.results_folder)
     print("\tOkapi URL:\t", args.okapi_url)
@@ -55,54 +177,19 @@ def main():
     print("\tUsername:   \t", args.username)
     print("\tPassword:   \tSecret")
     print("\tRecord source:\t", args.data_source)
-
-    files = [
-        f for f in listdir(args.source_folder) if isfile(join(args.source_folder, f))
-    ]
-    print("Files to process: {}".format(len(files)))
-    # print(json.dumps(files, sort_keys=True, indent=4))
     folio_client = FolioClient(
         args.okapi_url, args.tenant_id, args.username, args.password
     )
-    mapper = RulesMapper(folio_client, args.results_folder)
-    print("Starting")
-    print("Rec./s\t\tTot. recs\t\t")
-    failed_records = list()
-    failed_files = list()
-    with open(results_file, "w+") as results_file:
-        processor = MarcProcessor(mapper, folio_client, results_file, args)
-        for file_name in files:
-            try:
-                with open(join(sys.argv[1], file_name), "rb") as marc_file:
-                    reader = MARCReader(marc_file, "rb", permissive=True)
-                    reader.hide_utf8_warnings = True
-                    if args.force_utf_8:
-                        print("FORCE UTF-8 is set to TRUE")
-                        reader.force_utf8 = True
-                    print("running {}".format(file_name))
-                    for record in reader:
-                        if record is None:
-                            print(
-                                "Current chunk: ",
-                                reader.current_chunk,
-                                " was ignored because the following exception raised: ",
-                                reader.current_exception,
-                            )
-                            failed_records.append(reader.current_chunk)
-                        else:
-                            processor.process_record(record)
-            except Exception as exception:
-                print(exception)
-                traceback.print_exc()
-                print(file_name)
-        # wrap up
-    print("Done. Wrapping up...")
-    processor.wrap_up()
-    print("Failed files:")
-    print(json.dumps(failed_files, sort_keys=True, indent=4))
-    print(f"Failed records ({len(failed_records)}):")
-    print(json.dumps(failed_records, indent=4))
-    print("done")
+    # Iniiate Worker
+    worker = Worker(folio_client, results_file, args)
+    worker.work()
+
+
+def add_stats(stats, a):
+    if a not in stats:
+        stats[a] = 1
+    else:
+        stats[a] += 1
 
 
 if __name__ == "__main__":
