7944f0433d4896a4c00278f5f195912eea9a1e6d
Theodor Tolstoy <ttolstoy@ebsco.com>
2021-02-16 15:10:11 +0100

    fix type on call number types

--- marc_to_folio/conditions.py
+++ marc_to_folio/conditions.py
@@ -322,7 +322,7 @@ class Conditions:
         if not self.default_call_number_type:
             self.default_call_number_type = next(
                 ct
-                for ct in self.folio.default_call_number_types
+                for ct in self.call_number_types
                 if ct["name"] == "Other scheme"
             )
         first_level_map = {
