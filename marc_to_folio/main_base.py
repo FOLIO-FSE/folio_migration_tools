53739aef8f84234cc2b1273634af67524fe6f12b
Theodor Tolstoy <ttolstoy@ebsco.com>
2021-04-15 10:15:47 +0200

    change formatter for log_file

--- marc_to_folio/main_base.py
+++ marc_to_folio/main_base.py
@@ -18,11 +18,12 @@ class MainBase:
         logger.addHandler(stream_handler)
 
         if log_file_path:
+            file_formatter = logging.Formatter("%(message)s")
             file_handler = logging.FileHandler(
                 filename=log_file_path
             )
             # file_handler.addFilter(LevelFilter(0, 20))
-            file_handler.setFormatter(formatter)
+            file_handler.setFormatter(file_formatter)
             file_handler.setLevel(logging.ERROR)
             logging.getLogger().addHandler(file_handler)
         logger.info("Logging setup")
