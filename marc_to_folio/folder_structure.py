5291193081e96f1100ed9cc62f2ebb977b5a8f47
Theodor Tolstoy <ttolstoy@ebsco.com>
2021-07-27 20:06:48 +0200

    missing piece of hrid

--- marc_to_folio/folder_structure.py
+++ marc_to_folio/folder_structure.py
@@ -4,7 +4,7 @@ import time
 
 
 class FolderStructure:
-    def __init__(self, base_path: Path, time_stamp:str):
+    def __init__(self, base_path: Path, time_stamp: str):
         print("Setting up folder structure", flush=True)
         self.time_stamp = time_stamp
         self.base_folder = Path(base_path)
@@ -24,13 +24,13 @@ class FolderStructure:
 
         self.results_folder = self.base_folder / "results"
         verify_folder(self.results_folder)
-        
+
         self.reports_folder = self.base_folder / "reports"
-        verify_folder(self.reports_folder)        
+        verify_folder(self.reports_folder)
 
         self.mapping_files_folder = self.base_folder / "mapping_files"
         verify_folder(self.mapping_files_folder)
-        
+
         gitignore = self.base_folder / ".gitignore"
         verify_git_ignore(gitignore)
 
@@ -39,11 +39,13 @@ class FolderStructure:
         logging.info("Git ignore is set up correctly")
         logging.info(f"Base folder is {self.base_folder}")
         logging.info(f"Reports and logs folder is {self.reports_folder}")
-        logging.info(f"Results folder is {self.results_folder}")        
+        logging.info(f"Results folder is {self.results_folder}")
         logging.info(f"Data folder is {self.data_folder}")
         logging.info(f"Source records files folder is {self.legacy_records_folder}")
 
-    def setup_migration_file_structure(self, object_type:str, source_file_type:str = ""):
+    def setup_migration_file_structure(
+        self, object_type: str, source_file_type: str = ""
+    ):
         if source_file_type:
             self.legacy_records_folder = self.data_folder / source_file_type
         else:
@@ -55,12 +57,11 @@ class FolderStructure:
         )
 
         self.transformation_extra_data_path = (
-            self.results_folder / f"{object_type}_transformation_{self.time_stamp}.extradata"
+            self.results_folder
+            / f"{object_type}_transformation_{self.time_stamp}.extradata"
         )
 
-        logging.info(
-            f"Log file will be located at {self.transformation_log_path}"
-        )
+        logging.info(f"Log file will be located at {self.transformation_log_path}")
 
         self.created_objects_path = (
             self.results_folder / f"folio_{object_type}_{self.time_stamp}.json"
@@ -71,36 +72,53 @@ class FolderStructure:
         )
 
         self.migration_reports_file = (
-            self.reports_folder / f"{object_type}_transformation_report_{self.time_stamp}.md"
+            self.reports_folder
+            / f"{object_type}_transformation_report_{self.time_stamp}.md"
         )
-        logging.info(f"{object_type} migration report file will be saved at {self.migration_reports_file}")
+        logging.info(
+            f"{object_type} migration report file will be saved at {self.migration_reports_file}"
+        )
 
         self.srs_records_path = self.results_folder / f"srs_{self.time_stamp}.json"
-        self.instance_id_map_path = self.results_folder / f"instance_id_map_{self.time_stamp}.json"
-        self.holdings_from_bibs_path = self.results_folder / f"holdings_from_bibs_{self.time_stamp}.json"
+        self.instance_id_map_path = (
+            self.results_folder / f"instance_id_map_{self.time_stamp}.json"
+        )
+        self.holdings_from_bibs_path = (
+            self.results_folder / f"holdings_from_bibs_{self.time_stamp}.json"
+        )
 
-        self.holdings_from_csv_path = self.results_folder / f"holdings_from_csv_{self.time_stamp}.json"
-        self.holdings_id_map_path = self.results_folder / f"holdings_id_map_{self.time_stamp}.json"
+        self.holdings_from_csv_path = (
+            self.results_folder / f"holdings_from_csv_{self.time_stamp}.json"
+        )
+        self.holdings_id_map_path = (
+            self.results_folder / f"holdings_id_map_{self.time_stamp}.json"
+        )
 
-        
-        self.holdings_from_mfhd_path = self.results_folder / f"holdings_from_bibs_{self.time_stamp}.json"
-        self.holdings_from_c_records_path = self.results_folder / f"holdings_from_bibs_{self.time_stamp}.json"
+        self.holdings_from_mfhd_path = (
+            self.results_folder / f"holdings_from_bibs_{self.time_stamp}.json"
+        )
+        self.holdings_from_c_records_path = (
+            self.results_folder / f"holdings_from_bibs_{self.time_stamp}.json"
+        )
 
         # Mapping files
-        self.locations_map_path = self.mapping_files_folder / 'locations.tsv'
-        self.temp_locations_map_path = self.mapping_files_folder / 'temp_locations.tsv'
+        self.locations_map_path = self.mapping_files_folder / "locations.tsv"
+        self.temp_locations_map_path = self.mapping_files_folder / "temp_locations.tsv"
         self.mfhd_rules_path = self.mapping_files_folder / "mfhd_rules.json"
         self.items_map_path = self.mapping_files_folder / "item_mapping.json"
-        self.holdings_map_path = self.mapping_files_folder / "holdingsrecord_mapping.json"
-        self.material_type_map_path = self.mapping_files_folder /"material_types.tsv"
-        self.loan_type_map_path = self.mapping_files_folder /"loan_types.tsv"
-        self.temp_loan_type_map_path = self.mapping_files_folder /"temp_loan_types.tsv"
-        self.call_number_type_map_path = self.mapping_files_folder /"call_number_type_mapping.tsv"
-        self.statistical_codes_map_path = self.mapping_files_folder /"statcodes.tsv"
-        self.item_statuses_map_path = self.mapping_files_folder /"item_statuses.tsv"
-        
+        self.holdings_map_path = (
+            self.mapping_files_folder / "holdingsrecord_mapping.json"
+        )
+        self.material_type_map_path = self.mapping_files_folder / "material_types.tsv"
+        self.loan_type_map_path = self.mapping_files_folder / "loan_types.tsv"
+        self.temp_loan_type_map_path = self.mapping_files_folder / "temp_loan_types.tsv"
+        self.call_number_type_map_path = (
+            self.mapping_files_folder / "call_number_type_mapping.tsv"
+        )
+        self.statistical_codes_map_path = self.mapping_files_folder / "statcodes.tsv"
+        self.item_statuses_map_path = self.mapping_files_folder / "item_statuses.tsv"
 
-        
+
 def verify_git_ignore(gitignore: Path):
     with open(gitignore, "a+") as f:
         contents = f.read()
@@ -117,7 +135,8 @@ def verify_git_ignore(gitignore: Path):
 
 def verify_folder(folder_path: Path):
     if not folder_path.is_dir():
-        logging.critical(f"There is no folder located at {folder_path}.Exiting.")
+        logging.critical(f"There is no folder located at {folder_path}. Exiting.")
+        logging.critical(f"Create a folder by calling\n\tmkdir {folder_path}")
         exit()
     else:
         print(f"Located {folder_path}", flush=True)
