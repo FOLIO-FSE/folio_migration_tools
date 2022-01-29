1cee21c4b37e8f6761da7f2276f865cd2bdc23bc
Jeremy Nelson <jermnelson@gmail.com>
2022-01-29 15:11:18 -0700

    Fixes import

--- migration_tools/marc_rules_transformation/bibs_processor.py
+++ migration_tools/marc_rules_transformation/bibs_processor.py
@@ -7,7 +7,7 @@ from folio_uuid.folio_uuid import FOLIONamespaces, FolioUUID
 from folioclient import FolioClient
 from migration_tools.custom_exceptions import TransformationRecordFailedError
 from migration_tools.folder_structure import FolderStructure
-from migration_tools.folio_releases import FOLIOReleases
+from migration_tools.library_configuration import FolioRelease
 from migration_tools.helper import Helper
 from migration_tools.library_configuration import FolioRelease
 from migration_tools.marc_rules_transformation.rules_mapper_bibs import BibsRulesMapper
