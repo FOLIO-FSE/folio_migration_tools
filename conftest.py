a3f705ae5098c0a706a7af86999ac750928afca2
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-01-31 16:27:39 +0100

    add conftest.py back

---
+++ conftest.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption("--password", action="store")
+    parser.addoption("--tenant_id", action="store")
+    parser.addoption("--okapi_url", action="store")
+    parser.addoption("--username", action="store")
