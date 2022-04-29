8cb534ec86707174d4fcaa47fd33d3562e9d2a5b
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-04-29 09:24:13 +0200

    add leader validation for  10 and 11 as well

--- src/folio_migration_tools/migration_tasks/bibs_transformer.py
+++ src/folio_migration_tools/migration_tasks/bibs_transformer.py
@@ -157,12 +157,25 @@ class BibsTransformer(MigrationTaskBase):
     def set_leader(marc_record: Record, migration_report: MigrationReport):
         if marc_record.leader[9] != "a":
             migration_report.add(
-                Blurbs.LeaderManipulation, f"Set leader09 from {marc_record.leader[9]} to a"
+                Blurbs.LeaderManipulation,
+                f"Set leader 09 (Character coding scheme) from {marc_record.leader[9]} to a",
             )
             marc_record.leader = f"{marc_record.leader[:9]}a{marc_record.leader[10:]}"
         if not marc_record.leader.endswith("4500"):
             migration_report.add(
                 Blurbs.LeaderManipulation,
-                f"Set leader 20-23 from {marc_record.leader[:4]} to 4500",
+                f"Set leader 20-23 from {marc_record.leader[-4:]} to 4500",
             )
             marc_record.leader = f"{marc_record.leader[:-4]}4500"
+        if marc_record.leader[10] != "2":
+            migration_report.add(
+                Blurbs.LeaderManipulation,
+                f"Set leader 10 (Indicator count) from {marc_record.leader[10]} to 2",
+            )
+            marc_record.leader = f"{marc_record.leader[:10]}2{marc_record.leader[11:]}"
+        if marc_record.leader[11] != "2":
+            migration_report.add(
+                Blurbs.LeaderManipulation,
+                f"Set leader 10 (Subfield code count) from {marc_record.leader[11]} to 2",
+            )
+            marc_record.leader = f"{marc_record.leader[:11]}2{marc_record.leader[12:]}"
