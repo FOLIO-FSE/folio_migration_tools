3277287d49be4aa1570c5107058cb0e3306c958e
Lisa Sjogren <lsjogren@corp.epnet.com>
2022-05-07 01:41:01 +0200

    assert that schema is a schema

--- tests/test_organization_mapper.py
+++ tests/test_organization_mapper.py
@@ -4,11 +4,8 @@ from folio_migration_tools.custom_exceptions import TransformationProcessError
 from folio_migration_tools.mapping_file_transformation.organization_mapper import OrganizationMapper
 
 
-def test_fetch_schema_from_github():
+def test_fetch_acq_schemas_from_github_happy_path():
     organization_schema = OrganizationMapper.get_latest_acq_schemas_from_github(
         "folio-org", "mod-organizations-storage", "mod-orgs", "organization")
 
-assert organization_schema
-#     folio_rec = {"circulationNotes": [{"id": "someId", "noteType": "Check in", "note": ""}]}
-#     ItemsTransformer.handle_circiulation_notes(folio_rec, str(uuid.uuid4()))
-#     assert "circulationNotes" not in folio_rec
+    assert organization_schema["$schema"]
