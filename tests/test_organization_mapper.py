7f10e5bc4018208b441435486ba2c72ab9c95b28
Lisa Sjogren <lsjogren@corp.epnet.com>
2022-05-06 21:57:15 +0200

    write test

--- tests/test_organization_mapper.py
+++ tests/test_organization_mapper.py
@@ -5,9 +5,10 @@ from folio_migration_tools.mapping_file_transformation.organization_mapper impor
 
 
 def test_fetch_schema_from_github():
-    organization_schema = OrganizationMapper.get_latest_acq_schema_from_github("mod-orgs", "organization")
+    organization_schema = OrganizationMapper.get_latest_acq_schemas_from_github(
+        "folio-org", "mod-organizations-storage", "mod-orgs", "organization")
 
-    assert organization_schema
+assert organization_schema
 #     folio_rec = {"circulationNotes": [{"id": "someId", "noteType": "Check in", "note": ""}]}
 #     ItemsTransformer.handle_circiulation_notes(folio_rec, str(uuid.uuid4()))
 #     assert "circulationNotes" not in folio_rec
