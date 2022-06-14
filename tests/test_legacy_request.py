462b35ec207617cd3883a51d5b24058035fdb304
Brooks Travis <brooks.travis@gmail.com>
2022-06-13 23:51:49 -0500

    Adding DST support for request loader

--- tests/test_legacy_request.py
+++ tests/test_legacy_request.py
@@ -1,4 +1,5 @@
 import uuid
+from zoneinfo import ZoneInfo
 
 import pytest
 
@@ -53,3 +54,17 @@ def test_serialize_success_kiwi():
     legacy_request.item_id = str(uuid.uuid4())
     legacy_request.patron_id = str(uuid.uuid4())
     assert legacy_request.serialize(FolioRelease.kiwi)
+
+def test_request_dates_to_utc():
+    legacy_request_dict = {
+        "item_barcode": "ib",
+        "patron_barcode": "pb",
+        "request_date": "2022-09-01",
+        "request_expiration_date": "2022-10-01",
+        "comment": "comment",
+        "request_type": "Hold",
+        "pickup_servicepoint_id": str(uuid.uuid4()),
+    }
+    legacy_request = LegacyRequest(legacy_request_dict, ZoneInfo("America/Chicago"))
+    assert legacy_request.request_date.isoformat() == "2022-09-01T05:00:00+00:00"
+    assert legacy_request.request_expiration_date.isoformat() == "2022-10-02T04:59:00+00:00"
