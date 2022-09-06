3beefdcba78c942ec0d7ef430046a353b55b056e
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-09-06 22:24:18 +0200

    add test for log data issue

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,13 @@
-from requests.exceptions import HTTPError
+import logging
+
 import pytest
 from folioclient import FolioClient
+from requests.exceptions import HTTPError
+
+from folio_migration_tools.helper import Helper
+
+LOGGER = logging.getLogger(__name__)
+LOGGER.propagate = True
 
 
 def test_get_latest_from_github_returns_none_when_failing():
@@ -25,3 +32,12 @@ def test_get_latest_from_github_returns_file_orgs_has_no_releases():
             "acq-models",
             "/mod-orgs/schemas/organization.json",
         )
+
+
+def test_log_dats_issue(caplog):
+    caplog.set_level(26)
+    Helper.log_data_issue("test id", "test log message", "legacy value")
+    assert "test log message" in caplog.text
+    assert "test id" in caplog.text
+    assert "legacy value" in caplog.text
+    # logging.log(26, "DATA ISSUE\t%s\t%s\t%s", index_or_id, message, legacy_value)
