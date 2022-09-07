40cb4b6b09214ecaeae85d134b8fe4754317621b
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-09-07 09:17:46 +0200

    move folioclient tests to folioclient

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,37 +1,9 @@
 import logging
 
-import pytest
-from folioclient import FolioClient
-from requests.exceptions import HTTPError
-
 from folio_migration_tools.helper import Helper
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.propagate = True
-
-
-def test_get_latest_from_github_returns_none_when_failing():
-    with pytest.raises(HTTPError):
-        FolioClient.get_latest_from_github("branchedelac", "tati", "myfile.json")
-
-
-def test_get_latest_from_github_returns_file_1():
-    schema = FolioClient.get_latest_from_github(
-        "folio-org",
-        "mod-source-record-manager",
-        "/mod-source-record-manager-server/src/main/resources/rules/marc_holdings_rules.json",
-    )
-    assert schema is not None
-    assert schema.get("001", None) is not None
-
-
-def test_get_latest_from_github_returns_file_orgs_has_no_releases():
-    with pytest.raises(HTTPError):
-        FolioClient.get_latest_from_github(
-            "folio-org",
-            "acq-models",
-            "/mod-orgs/schemas/organization.json",
-        )
 
 
 def test_log_dats_issue(caplog):
@@ -40,4 +12,3 @@ def test_log_dats_issue(caplog):
     assert "test log message" in caplog.text
     assert "test id" in caplog.text
     assert "legacy value" in caplog.text
-    # logging.log(26, "DATA ISSUE\t%s\t%s\t%s", index_or_id, message, legacy_value)
