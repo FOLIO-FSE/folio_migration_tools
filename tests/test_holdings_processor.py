dfd63ddf8a1f50f502c01ccfdab41bc4c697709a
Brooks Travis <brooks.travis@gmail.com>
2022-09-26 00:36:06 -0500

    Adding tests for generate_num_part method

--- tests/test_holdings_processor.py
+++ tests/test_holdings_processor.py
@@ -1,7 +1,12 @@
+from unittest.mock import Mock
+
 from pymarc import Field, Record
 
 from folio_migration_tools.marc_rules_transformation.holdings_processor import (
     HoldingsProcessor,
+)
+from folio_migration_tools.marc_rules_transformation.rules_mapper_holdings import (
+    RulesMapperHoldings,
 )
 
 
@@ -61,3 +66,21 @@ def test_get_legacy_id_001_right_order():
     record.add_field(Field(tag="001", data="0001"))
     legacy_id = HoldingsProcessor.get_legacy_id("951$c", record)
     assert legacy_id == "cid"
+
+
+def test_generate_num_part_retain_leading_zeroes():
+    mock_processor = Mock(spec=HoldingsProcessor)
+    mock_mapper = Mock(spec=RulesMapperHoldings)
+    mock_mapper.holdings_hrid_counter = "1"
+    mock_mapper.common_retain_leading_zeroes = True
+    mock_processor.mapper = mock_mapper
+    assert HoldingsProcessor.generate_num_part(mock_processor) == "00000000001"
+
+
+def test_generate_num_part_without_leading_zeroes():
+    mock_processor = Mock(spec=HoldingsProcessor)
+    mock_mapper = Mock(spec=RulesMapperHoldings)
+    mock_mapper.holdings_hrid_counter = "1"
+    mock_mapper.common_retain_leading_zeroes = False
+    mock_processor.mapper = mock_mapper
+    assert HoldingsProcessor.generate_num_part(mock_processor) == "1"
