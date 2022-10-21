27ea530f47fa7f069280330c90db0191565fa955
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-10-21 10:27:06 +0200

    finish off

--- src/folio_migration_tools/mapper_base.py
+++ src/folio_migration_tools/mapper_base.py
@@ -34,13 +34,15 @@ class MapperBase:
         self.hrid_path = "/hrid-settings-storage/hrid-settings"
         self.library_configuration: LibraryConfiguration = library_configuration
         self.hrid_settings = self.folio_client.folio_get_single_object(self.hrid_path)
-        self.instance_hrid_prefix = self.hrid_settings["instances"]["prefix"]
-        self.instance_hrid_counter = self.hrid_settings["instances"]["startNumber"]
-        self.holdings_hrid_prefix = self.hrid_settings["holdings"]["prefix"]
-        self.holdings_hrid_counter = self.hrid_settings["holdings"]["startNumber"]
-        self.items_hrid_prefix = self.hrid_settings["items"]["prefix"]
-        self.items_hrid_counter = self.hrid_settings["items"]["startNumber"]
-        self.common_retain_leading_zeroes: bool = self.hrid_settings["commonRetainLeadingZeroes"]
+        self.instance_hrid_prefix = self.hrid_settings["instances"].get("prefix", "")
+        self.instance_hrid_counter = self.hrid_settings["instances"].get("startNumber", 1)
+        self.holdings_hrid_prefix = self.hrid_settings["holdings"].get("prefix", "")
+        self.holdings_hrid_counter = self.hrid_settings["holdings"].get("startNumber", 1)
+        self.items_hrid_prefix = self.hrid_settings["items"].get("prefix", "")
+        self.items_hrid_counter = self.hrid_settings["items"].get("startNumber", 1)
+        self.common_retain_leading_zeroes: bool = self.hrid_settings.get(
+            "commonRetainLeadingZeroes", True
+        )
 
         logging.info("Fetched HRID settings.")
         logging.info("Instance HRID prefix is %s", self.instance_hrid_prefix)
