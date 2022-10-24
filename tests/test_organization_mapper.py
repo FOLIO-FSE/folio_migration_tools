04ef738bdcdc72e328078ca44a410a9062ed66cd
branchedelac <lisa.m.sjogren@gmail.com>
2022-10-24 14:06:05 +0200

    refine tests

--- tests/test_organization_mapper.py
+++ tests/test_organization_mapper.py
@@ -52,9 +52,9 @@ def mapper(pytestconfig) -> OrganizationMapper:
         okapi_username=username,
         okapi_password=password,
         folio_release=FolioRelease.lotus,
-        library_name="Test Run Library",
+        library_name="Organization tester Library",
         log_level_debug=False,
-        iteration_identifier="I have no clue",
+        iteration_identifier="Test!",
         base_folder="/",
         multi_field_delimiter="^-^",
     )
@@ -65,8 +65,19 @@ def mapper(pytestconfig) -> OrganizationMapper:
     ]
 
     email_categories_map = [
-        {"email_categories": "tspt", "folio_value": "Technical Support"},
-        {"email_categories": "*", "folio_value": "General"},
+        {
+            "email1_categories": "tspt",
+            "email2_categories": "*",
+            "folio_value": "Technical Support",
+        },
+        {"email1_categories": "sls", "email2_categories": "*", "folio_value": "Sales"},
+        {
+            "email1_categories": "*",
+            "email2_categories": "tspt",
+            "folio_value": "Technical Support",
+        },
+        {"email1_categories": "*", "email2_categories": "sls", "folio_value": "Sales"},
+        {"email1_categories": "*", "email_2categories": "*", "folio_value": "General"},
     ]
 
     phone_categories_map = [
@@ -76,13 +87,13 @@ def mapper(pytestconfig) -> OrganizationMapper:
 
     organization_types_map = [
         {"organization_types": "cst", "folio_name": "Consortium"},
-        {"organization_types": "*", "folio_name": "General"},
+        {"organization_types": "*", "folio_name": "Unspecified"},
     ]
 
     return OrganizationMapper(
         mock_folio_client,
         lib_config,
-        basic_organization_map,
+        organization_map,
         organization_types_map,
         address_categories_map,
         email_categories_map,
@@ -90,75 +101,105 @@ def mapper(pytestconfig) -> OrganizationMapper:
     )
 
 
-def test_basic_mapping(mapper, caplog):
-    # caplog.set_level(25)
-    data = {
-        "vendor_code": "AbeBooks",
-        "ACCTNUM": "aha112233",
-        "VENNAME": "Abe Books",
-        "EMAIL": "email1@abebooks.com",
-        "email_categories": "aspt",
-        "EMAIL2": "email2@abebooks.com",
-        "PHONE NUM": "123-456",
-        "phone_categories": "mspt",
-        "Alt name type": "Nickname",
-        "Alternative Names": "Abby",
-        "status": "Active",
-        "org_note": "Good stuff!",
-        "address_line_1": "Suite 500 - 655 Typee Rd",
-        "address_city": "Victoria",
-        "address_categories": "rt^-^ad",
-        "tp": "Consortium",
-        "tgs": "A, B, C",
-        "organization_types": "cst^-^aah",
-    }
+def test_parse_record_mapping_file(mapper):
+    folio_keys = MappingFileMapperBase.get_mapped_folio_properties_from_map(organization_map)
+
+    assert folio_keys
+
+
+def test_organization_mapping(mapper):
 
     organization, idx = mapper.do_map(data, data["vendor_code"], FOLIONamespaces.organizations)
 
-    # TODO Add tests for extradata
-    # mapper.perform_additional_mappings(res)
-    # mapper.store_objects(res)
-    # assert "Level 25" in caplog.text
-    # assert "organization\t" in caplog.text
-    # generated_objects = {}
-    # for m in caplog.messages:
-    #     s = m.split("\t")
-    #     generated_objects[s[0]] = json.loads(s[1])
-    # organization = generated_objects["organization"]
-
-    # Test basic values
+    # Test string values mapping
     assert organization["name"] == "Abe Books"
+    assert organization["code"] == "AbeBooks"
+    assert organization["description"] == "Good stuff!"
+    assert organization["status"] == "Active"
+    assert organization["accounts"][0]["accountNo"] == "aha112233"
 
-    # Test reference data mappings
-    assert organization["organizationTypes"] == ["76b06c55-d95a-4ae0-a47d-5708f4e5e166"]
+
+def test_single_org_type_refdata_mapping(mapper):
+
+    organization, idx = mapper.do_map(data, data["vendor_code"], FOLIONamespaces.organizations)
+
+    # Test reference data mapping
+    assert organization["organizationTypes"] == ["837d04b6-d81c-4c49-9efd-2f62515999b3"]
+
+
+def test_single_category_refdata_mapping(mapper):
+
+    organization, idx = mapper.do_map(data, data["vendor_code"], FOLIONamespaces.organizations)
 
     # Test arrays of contact information
-    assert organization["addresses"][0]["categories"] == ["76b06c55-d95a-4ae0-a47d-5708f4e5e166"]
+    assert organization["addresses"][0]["categories"] == ["c78640d5-a1ec-4721-9a1f-c6f876d4c179"]
 
     assert organization["phoneNumbers"][0]["categories"] == [
-        "87042758-5266-472a-a3e9-ea1ca0ccf056"
+        "e193b0d1-4674-4a9e-818b-375f013d963f"
     ]
 
-    # TODO Sort out the below index-specific tests which are currently failing because items are
-    # added to the emails array in random order.
-    # assert organization["emails"][0]["value"] == "email1@abebooks.com"
-    # assert organization["emails"][0]["isPrimary"]
-    # assert organization["emails"][0]["categories"] == ["93042758-5266-472a-a3e9-ea1ca0ccf056"]
 
-    # assert organization["emails"][1]["value"] == "email2@abebooks.com"
-    # assert not organization["emails"][1]["isPrimary"]
-    # assert organization["emails"][1]["categories"] == [""]
+@pytest.mark.skip(
+    reason="We would need a way of using the same ref data file for multiple values. See #411"
+)
+def test_multiple_emails_array_objects(mapper):
+    organization, idx = mapper.do_map(data, data["vendor_code"], FOLIONamespaces.organizations)
+
+    correct_email_objects = 0
+
+    for email in organization["emails"]:
+        # One of the email addresses
+        if (
+            email["value"] == "email1@abebooks.com"
+            and email["categories"][0] == "604c2c9d-ed3a-46cd-bec4-69926c303b22"
+            and email["isPrimary"]
+        ):
+            correct_email_objects += 1
+
+        # One of the email addresses
+        if (
+            email["value"] == "email2@abebooks.com"
+            and email["categories"][0] == "97dcb23df-1aba-444e-b88d-804d17c715a5"
+            and not email["isPrimary"]
+        ):
+            correct_email_objects += 1
+
+    assert correct_email_objects == 2
 
 
-def test_parse_record_mapping_file(mapper):
-    folio_keys = MappingFileMapperBase.get_mapped_folio_properties_from_map(
-        basic_organization_map
-    )
-    
-    assert folio_keys
+@pytest.mark.skip(reason="Extra data has not been implemented in the mapper yet.")
+def test_extra_data(mapper, caplog):
+    organization, idx = mapper.do_map(data, data["vendor_code"], FOLIONamespaces.organizations)
+
+    # TODO Add tests for extradata. It should
+
+
+# Shared data and maps
+data = {
+    "vendor_code": "AbeBooks",
+    "ACCTNUM": "aha112233",
+    "VENNAME": "Abe Books",
+    "EMAIL": "email1@abebooks.com",
+    "email1_categories": "sls",
+    "EMAIL2": "email2@abebooks.com",
+    "email2_categories": "tspt",
+    "PHONE NUM": "123-456",
+    "phone_categories": "mspt",
+    "Alt name type": "Nickname",
+    "Alternative Names": "Abby",
+    "status": "Active",
+    "address_line_1": "Suite 500 - 655 Typee Rd",
+    "address_city": "Victoria",
+    "address_categories": "rt",
+    "tp": "Consortium",
+    "tgs": "A, B, C",
+    "organization_types": "cst",
+    "org_note": "Good stuff!",
+}
+
 
 # A mocked mapping file
-basic_organization_map = {
+organization_map = {
     "data": [
         {
             "folio_field": "accounts[0].accountNo",
@@ -215,8 +256,14 @@ basic_organization_map = {
             "description": "",
         },
         {
+            "folio_field": "description",
+            "legacy_field": "org_note",
+            "value": "",
+            "description": "",
+        },
+        {
             "folio_field": "emails[0].categories[0]",
-            "legacy_field": "email_categories",
+            "legacy_field": "email1_categories",
             "value": "",
             "description": "",
         },
@@ -246,9 +293,9 @@ basic_organization_map = {
         },
         {
             "folio_field": "emails[1].categories[0]",
-            "legacy_field": "Not mapped",
+            "legacy_field": "email2_categories",
             "value": "",
-            "description": "If we have multiple email addresses, how can that be mapped?",
+            "description": "",
         },
         {
             "folio_field": "isVendor",
