84f20e0628f6cdc29cbea29a87193144f02b12b8
Theodor Tolstoy <github.teddes@tolstoy.se>
2022-12-08 13:31:03 +0100

    update ref data generation

--- src/fetch_default_ref_data.py
+++ src/fetch_default_ref_data.py
@@ -6,6 +6,8 @@ import sys
 import requests
 
 super_schema = {
+    "/location-units": {},
+    "/locations": {},
     "/alternative-title-types": {},
     "/authority-note-types": {},
     "/call-number-types": {},
@@ -26,8 +28,6 @@ super_schema = {
     "/item-damaged-statuses": {},
     "/item-note-types": {},
     "/loan-types": {},
-    "/location-units": {},
-    "/locations": {},
     "/material-types": {},
     "/modes-of-issuance": {},
     "/nature-of-content-terms": {},
@@ -46,6 +46,21 @@ def main():
             "folio-org", "mod-inventory-storage", f"/reference-data{entry}"
         )
         print(f"fetched {entry}")
+        if entry == "/holdings-types":
+            res.append(
+                {
+                    "id": "b92480eb-210d-442e-a6f7-4043fe7f0f24",
+                    "name": "Unknown",
+                    "source": "local",
+                    "metadata": {
+                        "createdDate": "2022-12-08T09:55:58.067+00:00",
+                        "createdByUserId": "7cee216a-b298-4e7c-b678-8917e6b8edd3",
+                        "updatedDate": "2022-12-08T09:55:58.067+00:00",
+                        "updatedByUserId": "7cee216a-b298-4e7c-b678-8917e6b8edd3",
+                    },
+                }
+            )
+
         super_schema[entry] = res
     rules = get_ref_data_from_github_folder(
         "folio-org",
@@ -65,31 +80,37 @@ def main():
 
 def get_ref_data_from_github_folder(owner, repo, folder_path: str):
     ret_arr = []
-    logging.info("Using GITHB_TOKEN environment variable for Gihub API Access")
-    github_headers = {
-        "content-type": "application/json",
-        "User-Agent": "Folio Client (https://github.com/FOLIO-FSE/FolioClient)",
-        "authorization": f"token {os.environ.get('GITHUB_TOKEN')}",
-    }
+    try:
 
-    latest_path = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
-    req = requests.get(latest_path, headers=github_headers)
-    req.raise_for_status()
-    latest = json.loads(req.text)
-    # print(json.dumps(latest, indent=4))
-    latest_tag = latest["tag_name"]
-    path = f"https://api.github.com/repos/{owner}/{repo}/contents{folder_path}?ref={latest_tag}"
-    # print(latest_path)
-    req = requests.get(path, headers=github_headers)
-    req.raise_for_status()
-    file_list = [x["name"] for x in json.loads(req.text) if x["type"] == "file"]
-    github_path = "https://raw.githubusercontent.com"
-    for file_name in file_list:
-        file_path = f"{github_path}/{owner}/{repo}/{latest_tag}{folder_path}/{file_name}"
-        resp = requests.get(file_path, headers=github_headers)
-        resp.raise_for_status()
-        ret_arr.append(json.loads(resp.text))
-    logging.debug(folder_path)
+        logging.info("Using GITHB_TOKEN environment variable for Gihub API Access")
+        github_headers = {
+            "content-type": "application/json",
+            "User-Agent": "Folio Client (https://github.com/FOLIO-FSE/FolioClient)",
+            "authorization": f"token {os.environ.get('GITHUB_TOKEN')}",
+        }
+
+        latest_path = f"https://api.github.com/repos/{owner}/{repo}/releases/latest"
+        req = requests.get(latest_path, headers=github_headers)
+        req.raise_for_status()
+        latest = json.loads(req.text)
+        # print(json.dumps(latest, indent=4))
+        latest_tag = latest["tag_name"]
+        path = (
+            f"https://api.github.com/repos/{owner}/{repo}/contents{folder_path}?ref={latest_tag}"
+        )
+        # print(latest_path)
+        req = requests.get(path, headers=github_headers)
+        req.raise_for_status()
+        file_list = [x["name"] for x in json.loads(req.text) if x["type"] == "file"]
+        github_path = "https://raw.githubusercontent.com"
+        for file_name in file_list:
+            file_path = f"{github_path}/{owner}/{repo}/{latest_tag}{folder_path}/{file_name}"
+            resp = requests.get(file_path, headers=github_headers)
+            resp.raise_for_status()
+            ret_arr.append(json.loads(resp.text))
+        logging.debug(folder_path)
+    except Exception:
+        logging.error("Something went wrong")
     return ret_arr
 
 
