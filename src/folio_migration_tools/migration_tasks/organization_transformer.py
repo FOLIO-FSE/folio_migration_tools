f9f83320dcef84cc55a7893b3508f4266bb28064
branchedelac <lisa.m.sjogren@gmail.com>
2023-01-30 16:05:56 +0100

    make it work

--- src/folio_migration_tools/migration_tasks/organization_transformer.py
+++ src/folio_migration_tools/migration_tasks/organization_transformer.py
@@ -282,6 +282,8 @@ class OrganizationTransformer(MigrationTaskBase):
         if "notes" in record:
             pass
 
+        return record
+
     def create_linked_extradata_objects(self, record, embedded_object, extradata_object_type):
         """Creates extradata objects from embedded extradata objects,
         and replaces the embedde dobjects with UUIDs.
@@ -305,8 +307,8 @@ class OrganizationTransformer(MigrationTaskBase):
 
         # Check if this object has already been created
         matched_uuids = [
-            key
-            for key, value in self.embedded_extradata_object_cache.items()
+            value
+            for value in self.embedded_extradata_object_cache
             if value == embedded_object_to_cache
         ]
 
@@ -328,7 +330,7 @@ class OrganizationTransformer(MigrationTaskBase):
         # Append the contact UUID to the organization record
         record[extradata_object_type].append(extradata_object_uuid)
 
-        self.embedded_extradata_object_cache[extradata_object_uuid] = embedded_object_to_cache
+        self.embedded_extradata_object_cache.add(embedded_object_to_cache)
 
         return record
 
