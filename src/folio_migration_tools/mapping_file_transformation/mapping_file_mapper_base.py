bb7aca304d6ab6389f7a2ffeb93273f8ca538ba4
branchedelac <lisa.m.sjogren@gmail.com>
2023-02-10 18:55:47 +0100

    add todo

--- src/folio_migration_tools/mapping_file_transformation/mapping_file_mapper_base.py
+++ src/folio_migration_tools/mapping_file_transformation/mapping_file_mapper_base.py
@@ -469,15 +469,20 @@ class MappingFileMapperBase(MapperBase):
                             )
 
                     elif (
-                        prop in sub_properties and sub_properties[prop].get("type", "") == "object"
+                        prop in sub_properties and sub_properties[prop].get("type", "") == "object" 
                     ):
+                        # TODO Rewrite so that all tests succeed.
+                        # The above breaks some object-specific tests, but none
+                        # of the test in mapping_file_mapper_base.
                         sub_object = {}
                         for sub_prop in sub_properties[prop]["properties"].keys():
-                            prop_path = f"{prop_name}[{i}].{prop}.{sub_prop}"
-                            if prop_path in self.folio_keys:
-                                res = self.get_prop(legacy_object, prop_path, index_or_id)
-                                self.report_legacy_mapping(self.legacy_basic_property(sub_prop), True, True)
-                                sub_object[sub_prop] = res 
+                            if sub_prop.get("type", "") == "string":
+                                prop_path = f"{prop_name}[{i}].{prop}.{sub_prop}"
+                                if prop_path in self.folio_keys:
+                                    res = self.get_prop(legacy_object, prop_path, index_or_id)
+                                    self.report_legacy_mapping(self.legacy_basic_property(sub_prop), True, True)
+                                    sub_object[sub_prop] = res
+                        
                         temp_object[prop] = sub_object
 
             i = i + 1
