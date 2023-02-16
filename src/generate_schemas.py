33ce7c38f85ccec6b6e3cf472df1b3c8b2e456c1
Theodor Tolstoy <github.teddes@tolstoy.se>
2023-02-16 13:50:24 +0100

    add orders super-schema to generate schemas

--- src/generate_schemas.py
+++ src/generate_schemas.py
@@ -8,6 +8,9 @@ from folio_migration_tools.library_configuration import LibraryConfiguration
 from folio_migration_tools.mapping_file_transformation.courses_mapper import (
     CoursesMapper,
 )
+from folio_migration_tools.mapping_file_transformation.order_mapper import (
+    CompositeOrderMapper,
+)
 from folio_migration_tools.mapping_file_transformation.organization_mapper import (
     OrganizationMapper,
 )
@@ -31,10 +34,12 @@ def main():
     args = parse_args()
     with open(os.path.join(args.results_path, "LibraryConfigurationSchema.json"), "w") as outfile:
         outfile.write(LibraryConfiguration.schema_json(indent=4))
+        outfile.write("\n")
 
     for t in inheritors(migration_task_base.MigrationTaskBase):
         with open(os.path.join(args.results_path, f"{t.__name__}Schema.json"), "w") as outfile:
             outfile.write(t.TaskConfiguration.schema_json(indent=4))
+            outfile.write("\n")
 
     generate_extended_folio_object_schema(args)
 
@@ -61,10 +66,21 @@ def generate_extended_folio_object_schema(args):
     )
     with open(os.path.join(args.results_path, "compositeOrganizationSchema.json"), "w") as outfile:
         outfile.write(json.dumps(organization_schema, indent=4))
+        outfile.write("\n")
 
     courses_schema = CoursesMapper.get_composite_course_schema()
     with open(os.path.join(args.results_path, "compositeCoursesSchema.json"), "w") as outfile:
         outfile.write(json.dumps(courses_schema, indent=4))
+        outfile.write("\n")
+
+    composite_order_schema = CompositeOrderMapper.get_latest_acq_schemas_from_github(
+        "folio-org", "mod-orders", "mod-orders", "composite_purchase_order"
+    )
+    with open(
+        os.path.join(args.results_path, "compositePurchaseOrderLineSchema.json"), "w"
+    ) as outfile:
+        outfile.write(json.dumps(composite_order_schema, indent=4))
+        outfile.write("\n")
 
 
 if __name__ == "__main__":
