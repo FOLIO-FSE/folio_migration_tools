711e7ff597c6774c61cd3627b434b6c599e76000
branchedelac <lisa.m.sjogren@gmail.com>
2023-02-27 16:24:21 +0100

    post interface credentials

--- tests/test_batch_poster.py
+++ tests/test_batch_poster.py
@@ -1,3 +1,5 @@
+import json
+
 from folio_uuid.folio_namespaces import FOLIONamespaces
 
 from folio_migration_tools.migration_tasks import batch_poster
@@ -30,3 +32,12 @@ def test_get_unsafe_and_safe_endpoints():
         == "/item-storage/batch/synchronous-unsafe"
     )
     assert batch_poster.get_api_info("Items")["api_endpoint"] == "/item-storage/batch/synchronous"
+
+
+def test_get_extradata_endpoint_interface_credential():
+    extradata = 'interfaceCredential\t{"username": "wikiUsername", "password": "wikiPassword", "interfaceId": "7e131c38-5384-44ed-9f4a-da6ca2f36498", "id": "94357d5f-154d-445d-bd1a-454df2acffd3"}'
+    (object_name, data) = extradata.split("\t")
+
+    endpoint = batch_poster.get_extradata_endpoint(object_name, data)
+
+    assert endpoint == 'organizations-storage/interfaces/7e131c38-5384-44ed-9f4a-da6ca2f36498/credentials'
