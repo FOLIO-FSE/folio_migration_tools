30a457403f6bb22178b6022cecbd973b6c44adef
Clinton Bradford <cbradford@ebsco.com>
2023-09-12 22:06:24 -0700

    Support internationalization in tests

--- conftest.py
+++ conftest.py
@@ -4,3 +4,13 @@ def pytest_addoption(parser):
     parser.addoption("--okapi_url", action="store")
     parser.addoption("--username", action="store")
     # parser.addoption("--folio_release", action="store")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def install_l10n():
+    import i18n
+
+    i18n.set("file_format", "json")
+    i18n.set("skip_locale_root_data", True)
+    i18n.set("fallback", "en")
+    i18n.set("filename_format", "{locale}.{format}")
