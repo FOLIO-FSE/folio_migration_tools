13a420e3630f68db3ec1146e258b9bffb64a9abf
Clinton Bradford <cbradford@ebsco.com>
2023-10-18 07:32:43 -0700

    Change main script to load files chainably

--- src/folio_migration_tools/__main__.py
+++ src/folio_migration_tools/__main__.py
@@ -13,6 +13,7 @@ from folio_migration_tools.custom_exceptions import TransformationProcessError
 from folio_migration_tools.library_configuration import LibraryConfiguration
 from folio_migration_tools.migration_tasks import *  # noqa: F403, F401
 from folio_migration_tools.migration_tasks import migration_task_base
+from folio_migration_tools.config_file_load import merge_load
 
 import i18n
 
@@ -68,70 +69,67 @@ def main():
 
         args = parse_args(sys.argv[1:])
         i18n.set("locale", args.report_language)
-        with open(args.configuration_path) as config_file_path:
-            try:
-                config_file_humped = json.load(config_file_path)
-                config_file_humped["libraryInformation"]["okapiPassword"] = args.okapi_password
-                config_file_humped["libraryInformation"]["baseFolder"] = args.base_folder_path
-                config_file = humps.decamelize(config_file_humped)
-                library_config = LibraryConfiguration(**config_file["library_information"])
-                try:
-                    migration_task_config = next(
-                        t for t in config_file["migration_tasks"] if t["name"] == args.task_name
-                    )
-                except StopIteration:
-                    task_names = [t.get("name", "") for t in config_file["migration_tasks"]]
-                    print(
-                        f"Referenced task name {args.task_name} not found in the "
-                        f'configuration file. Use one of {", ".join(task_names)}'
-                        "\nHalting..."
-                    )
-                    sys.exit(2)
-                try:
-                    task_class = next(
-                        tc
-                        for tc in task_classes
-                        if tc.__name__ == migration_task_config["migration_task_type"]
-                    )
-                    task_config = task_class.TaskConfiguration(**migration_task_config)
-                    task_obj = task_class(task_config, library_config)
-                    task_obj.do_work()
-                    task_obj.wrap_up()
-                except StopIteration:
-                    print(
-                        f'Referenced task {migration_task_config["migration_task_type"]} '
-                        "is not a valid option. Update your task to incorporate "
-                        f"one of {json.dumps([tc.__name__ for tc in task_classes], indent=4)}"
-                    )
-                    sys.exit(2)
-                except TransformationProcessError as tpe:
-                    logging.critical(tpe.message)
-                    print(f"\n{tpe.message}: {tpe.data_value}")
-                    sys.exit(1)
-            except json.decoder.JSONDecodeError as json_error:
-                logging.critical(json_error)
-                print(json_error.doc)
-                print(
-                    f"\n{json_error}"
-                    f"\nError parsing the above JSON mapping or configruation file. Halting."
-                )
-                sys.exit(2)
-            except ValidationError as e:
-                print(e.json())
-                print("Validation errors in configuration file:")
-                print("==========================================")
+        config_file_humped = merge_load(args.configuration_path)
+        config_file_humped["libraryInformation"]["okapiPassword"] = args.okapi_password
+        config_file_humped["libraryInformation"]["baseFolder"] = args.base_folder_path
+        config_file = humps.decamelize(config_file_humped)
+        library_config = LibraryConfiguration(**config_file["library_information"])
+        try:
+            migration_task_config = next(
+                t for t in config_file["migration_tasks"] if t["name"] == args.task_name
+            )
+        except StopIteration:
+            task_names = [t.get("name", "") for t in config_file["migration_tasks"]]
+            print(
+                f"Referenced task name {args.task_name} not found in the "
+                f'configuration file. Use one of {", ".join(task_names)}'
+                "\nHalting..."
+            )
+            sys.exit(2)
+        try:
+            task_class = next(
+                tc
+                for tc in task_classes
+                if tc.__name__ == migration_task_config["migration_task_type"]
+            )
+            task_config = task_class.TaskConfiguration(**migration_task_config)
+            task_obj = task_class(task_config, library_config)
+            task_obj.do_work()
+            task_obj.wrap_up()
+        except StopIteration:
+            print(
+                f'Referenced task {migration_task_config["migration_task_type"]} '
+                "is not a valid option. Update your task to incorporate "
+                f"one of {json.dumps([tc.__name__ for tc in task_classes], indent=4)}"
+            )
+            sys.exit(2)
+        except TransformationProcessError as tpe:
+            logging.critical(tpe.message)
+            print(f"\n{tpe.message}: {tpe.data_value}")
+            sys.exit(1)
+        # task_obj.do_work()
+        logging.info("Work done. Shutting down")
+        sys.exit(0)
+        # task_obj.wrap_up()
+    except json.decoder.JSONDecodeError as json_error:
+        logging.critical(json_error)
+        print(json_error.doc)
+        print(
+            f"\n{json_error}"
+            f"\nError parsing the above JSON mapping or configruation file. Halting."
+        )
+        sys.exit(2)
+    except ValidationError as e:
+        print(e.json())
+        print("Validation errors in configuration file:")
+        print("==========================================")
 
-                for validation_message in json.loads(e.json()):
-                    print(
-                        f"{validation_message['msg']}\t"
-                        f"{', '.join(humps.camelize(str(x)) for x in validation_message['loc'])}"
-                    )
-                print("Halting")
-
-            # task_obj.do_work()
-            logging.info("Work done. Shutting down")
-            sys.exit(0)
-            # task_obj.wrap_up()
+        for validation_message in json.loads(e.json()):
+            print(
+                f"{validation_message['msg']}\t"
+                f"{', '.join(humps.camelize(str(x)) for x in validation_message['loc'])}"
+            )
+        print("Halting")
     except httpx.HTTPError as connection_error:
         print(
             f"\nConnection Error when connecting to {connection_error.request.url}. "
