306a8d52a5eb3e9029d5b1b6279e5a3465e363b9
Brooks Travis <brooks.travis@gmail.com>
2024-11-21 09:24:39 -0600

    Add folio_client to BatchPoster __init__ and super().__init__

--- src/folio_migration_tools/migration_tasks/batch_poster.py
+++ src/folio_migration_tools/migration_tasks/batch_poster.py
@@ -101,9 +101,10 @@ class BatchPoster(MigrationTaskBase):
         self,
         task_config: TaskConfiguration,
         library_config: LibraryConfiguration,
+        folio_client,
         use_logging: bool = True,
     ):
-        super().__init__(library_config, task_config, use_logging)
+        super().__init__(library_config, task_config, folio_client, use_logging)
         self.migration_report = MigrationReport()
         self.performing_rerun = False
         self.failed_ids: list = []
