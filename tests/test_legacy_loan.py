c8aa7777660972a67f362029fa07a731599108a7
Maksym Trineiev <mtrineyev@gmail.com>
2025-03-27 23:25:19 +0200

    correct_for_1_day_loans() covered with test

--- tests/test_legacy_loan.py
+++ tests/test_legacy_loan.py
@@ -1,7 +1,9 @@
+import pytest
 from zoneinfo import ZoneInfo
 
 from folio_migration_tools.migration_report import MigrationReport
 from folio_migration_tools.transaction_migration.legacy_loan import LegacyLoan
+from folio_migration_tools.custom_exceptions import TransformationProcessError
 
 
 def test_init():
@@ -239,3 +241,38 @@ def test_to_dict() -> None:
         "next_item_status": "Checked out",
     }
     assert legacy_loan.to_dict() == expected_result
+
+
+def test_correct_for_1_day_loans_time_alignment_is_ok() -> None:
+    loan_dict = {
+        "item_barcode": "the barcode with trailing spaces    ",
+        "patron_barcode": "   the barcode with leading spaces",
+        "due_date": "20250327",
+        "out_date": "20250327",
+        "renewal_count": "1",
+        "next_item_status": "Checked out",
+    }
+    tenant_timezone = ZoneInfo("UTC")
+    migration_report = MigrationReport()
+    legacy_loan = LegacyLoan(loan_dict, "", migration_report, tenant_timezone)
+    assert legacy_loan.due_date.isoformat() == "2025-03-27T23:59:00+00:00"
+    assert legacy_loan.out_date.isoformat() == "2025-03-27T00:01:00+00:00"
+
+
+def test_correct_for_1_day_loans_due_date_is_before_out_date() -> None:
+    loan_dict = {
+        "item_barcode": "the barcode with trailing spaces    ",
+        "patron_barcode": "   the barcode with leading spaces",
+        "due_date": "20250326",
+        "out_date": "20250327",
+        "renewal_count": "1",
+        "next_item_status": "Checked out",
+    }
+    tenant_timezone = ZoneInfo("UTC")
+    migration_report = MigrationReport()
+    expected_err_message = (
+        "Critical Process issue. Check configuration, mapping files and reference data\t"
+        "0\tDue date is before out date\t"
+    )
+    with pytest.raises(TransformationProcessError, match=expected_err_message):
+        LegacyLoan(loan_dict, "", migration_report, tenant_timezone)
