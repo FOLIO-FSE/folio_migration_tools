93670cb628020fb772ad0bc7f31b5ab0bad91926
Brooks Travis <brooks.travis@gmail.com>
2026-02-04 11:06:32 -0600

    Switch to `safety scan` for dependency vulnerability checks, and from black/flake8 to ruff for linting. Add docstrings linting rules, with some exclusions for later clean-up.
    - Ignore all docstrings rules in github publish workflow linting check

--- noxfile.py
+++ noxfile.py
@@ -7,7 +7,7 @@ import tempfile
 import nox
 
 nox.options.sessions = "lint", "safety", "tests", "docs"
-locations = "src", "tests", "noxfile.py", "docs/conf.py"
+locations = "src", "tests", "noxfile.py", "docs/source/conf.py"
 
 if "GITHUB_TOKEN" in os.environ:
     env = {"GITHUB_TOKEN": os.environ["GITHUB_TOKEN"]}
@@ -19,54 +19,30 @@ else:
 @nox.session()
 def tests(session):
     posargs = ["--cov=./", "--cov-report=xml", "--durations=20"]
-    session.run("poetry", "install", external=True)
-    session.run("pytest", *posargs, env=env)
+    session.run("uv", "sync", "--all-groups", "--all-extras", external=True)
+    session.run("uv", "run", "pytest", *posargs, env=env, external=True)
 
 
 @nox.session()
 def lint(session):
     args = session.posargs or locations
-    session.install(
-        "flake8",
-        "flake8-annotations",
-        "flake8-bandit",
-        "flake8-black",
-        "flake8-bugbear",
-        "flake8-docstrings",
-        "flake8-import-order",
-        "darglint",
-    )
-    session.run("flake8", *args)
+    session.run("uv", "run", "ruff", "check", "--fix", *args, external=True)
 
 
 @nox.session()
 def black(session):
     args = session.posargs or locations
-    session.install("black")
-    session.run("black", *args)
+    session.run("uv", "sync", "--all-groups", "--all-extras", external=True)
+    session.run("uv", "run", "ruff", "format", *args, external=True)
 
 
 @nox.session()
 def safety(session):
-    with tempfile.NamedTemporaryFile() as requirements:
-        session.run(
-            "poetry",
-            "export",
-            "--dev",
-            "--format=requirements.txt",
-            "--without-hashes",
-            f"--output={requirements.name}",
-            external=True,
-        )
-        session.install("safety")
-        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+    session.run("uvx", "safety", "scan", external=True)
 
 
 @nox.session()
 def docs(session) -> None:
     """Build the documentation."""
-    session.run("poetry", "install", "--no-dev", external=True)
-    session.install("sphinx")
-    session.install("sphinx-autodoc-typehints")
-    session.install("myst_parser")
-    session.run("sphinx-build", "docs", "docs/_build")
+    session.run("uv", "sync", "--group", "docs", external=True)
+    session.run("uv", "run", "sphinx-build", "docs/source", "docs/_build", external=True)
